976e839fb156e20958f9eb618ac8f38d6e108cda
Dylan Steck <dylancsteck@gmail.com>
2017-03-12 01:50:13 -0500

    gonna start collection view-- parse all done

--- WebDeck2017/controllers/HomepageViewController.swift
+++ WebDeck2017/controllers/HomepageViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 import Foundation
 import Parse
+import EventKit
+import EventKitUI
 
 class HomepageViewController: UIViewController {
-
+    var calendar = EKCalendar(for: .event, eventStore: EKEventStore())
+    var events: [EKEvent]?
+    var eventStore = EKEventStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(PFUser.current())
         print(PFUser.current()?["username"])
-        // Do any additional setup after loading the view.
+        
+        //Calling on the function that gets events from the user
+        loadEvents()
+        
+        //Sets date standards and formats for the calendar
+        let currentDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMM d, y"
+        let dateObj = formatter.string(from: currentDate)
+        
+        //Asking the phone for permission to EventStore, which is the place where the calendar and reminder data is stored(WebDeck would like to access your Calendar)
+        var eventStore = EKEventStore()
+        eventStore.requestAccess(to: EKEntityType.event,
+                                 completion: {(granted: Bool, error:NSError!) in
+                                    if !granted {
+                                        print("Access to store not granted")
+                                    }
+                                    } as! EKEventStoreRequestAccessCompletionHandler)
+    
     }
 
+    //Loads the user's events within one day.
+    func loadEvents() {
+        let calendar: Calendar = Calendar.current
+        var todayComponents: DateComponents = DateComponents()
+        todayComponents.day = 0
+        let today: Date! = (calendar as NSCalendar).date(byAdding: todayComponents, to: Date(), options: [])
+        var tomorrowComponents: DateComponents = DateComponents()
+        tomorrowComponents.day = 1
+        let oneMonthAfter = Date(timeIntervalSinceNow: +86400)
+        var tomorrow: Date! = (calendar as NSCalendar).date(byAdding: todayComponents, to: oneMonthAfter, options: [])
+        
+        let predicate: NSPredicate = eventStore.predicateForEvents(withStart: today, end: oneMonthAfter, calendars: nil)
+        // Fetch all events that match the predicate
+        let events: [AnyObject] = eventStore.events(matching: predicate)
+        //Gets those events
+        if events.isEmpty == true {
+//            myCoolLabel.text = "No events"
+            print("You have no events today.")
+        }
+        else {
+            print(events)
+            // Querrys through the events, prints them to the console, and logs it in Answers if there are events.
+            for event in events {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "MMM d h:mm a"
+                let dateObj = formatter.string(from: event.startDate)
+                let textToAppend = (" \(dateObj) - \(event.title!)") + "\r\n"
+//                self.myCoolLabel.text = self.myCoolLabel.text + textToAppend
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
